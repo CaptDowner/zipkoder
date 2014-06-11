bd0b21940ba692a30b3e3d94ada4b5abba47d449
CaptDowner <captdowner@gmail.com>
2014-06-11 11:24:07 -0700

    Added users to project.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609230223) do
+ActiveRecord::Schema.define(version: 20140610210742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "admin",           default: false
+    t.boolean  "grand_poobah"
+    t.boolean  "member"
+  end
 
   create_table "zips", force: true do |t|
     t.string   "city"
