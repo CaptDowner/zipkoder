2cd9dc49da42c287d9678ba93aacd07fe653a00a
CaptDowner <captdowner@gmail.com>
2014-06-12 14:24:06 -0700

    Added support dir.

---
+++ spec/support/attributes.rb
@@ -0,0 +1,23 @@
+def zip_attributes(overrides = {})
+  random_number = (0..9).to_a.shuffle[0..10].join
+  {
+    city:      "Alcatraz #{random_number}",
+    state:     "Zidaho",
+    state_2:   "ZO",
+    zip:       "99999",
+    lat:       "38.000000",
+    long:      "122.00000",
+    tz_offset: "+11:00",
+    dst:       "y"
+  }.merge(overrides)
+end
+
+def user_attributes(overrides = {})
+  {
+    firstname: "Example",
+    lastname: "User",
+    email: "user@example.com",
+    password: "secret",
+    password_confirmation: "secret"
+  }.merge(overrides)
+end
