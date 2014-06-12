36b819b2af07bc4478197a581ffb059c707b6d43
CaptDowner <captdowner@gmail.com>
2014-06-12 08:55:17 -0700

    Added validations; Defined self.authenticate method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :name, presence: true
+  # use Michael Hartl's regex for email validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :email, presence: true,
             format: /\A\S+@\S+\z/
   validates :email, presence: true,
-            format: /\A\S+@\S+\z/,
+            format: VALID_EMAIL_REGEX,
             uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 7, allow_blank: true }
 
+ def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    user && user.authenticate(password)
+  end
 
 end
