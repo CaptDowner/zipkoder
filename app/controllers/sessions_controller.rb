aa922acf1a28c4209a92d53717b71e9df73a3c26
CaptDowner <captdowner@gmail.com>
2014-06-26 07:15:51 -0700

    modified create and destroy methods for session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:user_id] = nil
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome back, #{user.firstname} #{user.lastname}!"
-#      redirect_to(session[:intended_url] || user)
-      redirect_to(zips_path)
-     # setup default session params
       session[:intended_url] = nil
       session[:query] = nil
       session[:sort] = 'zip'
       session[:direction] = 'asc'
+      flash[:notice] = "Welcome back, #{user.firstname} #{user.lastname}!"
+#      redirect_to(session[:intended_url] || user)
+      redirect_to(zips_path)
+     # setup default session params
     else
+#      binding.pry
       if(session[:user_id])
         render :new
       else
-        flash.now[:alert] = "Invalid email/password combination!"
+        flash[:alert] = "Invalid email/password combination!"
         render :new
       end
     end
@@ -25,6 +27,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+#    session[:current_user] = nil
+    self.current_user = nil
     redirect_to root_url, notice: "You're now signed out!"
   end
 
